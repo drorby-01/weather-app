cc86c2ba17c80b0014fb9ce055ecc7c921584b16
drorby-01 <drorby23@gmail.com>
2021-04-17 14:48:38 +0300

    weather-app

--- src/Weather.ts
+++ src/Weather.ts
@@ -4,7 +4,7 @@ import { IWheater } from "./redux/CountryWeather/CountryWeather.action";
 export class Weather {
   private static DEFAULTKEY: string; //tel aviv get location key
   private static DEFAULTCITY: string;
-  private static readonly APIKEY:string = "tOhfPPMdgKCbtHGMqPaM2vYAr8CtqRcs" 
+  private static readonly APIKEY:string = "Vs8RzarYGP6AtHn3zkeQz2iyJDC2GXeE" 
   
   private static async getLocationKey(cityName: string) {
     if (cityName) {
